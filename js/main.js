bf25dfdabf9011f8519c9a76aa38ce2edd9cde14
Paoquime <paola.qmelendez@gmail.com>
2016-09-14 17:00:17 -0500

    indentación

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
+document.getElementById("form-signin-heading").innerHTML = "Por favor inicia sesión";
 document.getElementById("inputEmail").placeholder="Correo Electrónico";
 document.getElementById("inputPassword").placeholder="Contraseña";
-document.getElementById("form-signin-heading").innerHTML = "Por favor inicia sesión";
 document.querySelector("span").innerHTML = "Recordar datos";
 document.querySelector("button").innerHTML = "Iniciar Sesión";
