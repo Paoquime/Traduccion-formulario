011b2429329c889a0852202b882540e0d3e40e9d
Paoquime <paola.qmelendez@gmail.com>
2016-09-14 14:18:08 -0500

    agregando cambios en button, título e input

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 document.getElementById("inputEmail").placeholder="Correo Electrónico";
 document.getElementById("inputPassword").placeholder="Contraseña";
+document.getElementById("form-signin-heading").innerHTML = "Por favor inicia sesión";
+document.querySelector("span").innerHTML = "Recordar datos";
+document.querySelector("button").innerHTML = "Iniciar Sesión";
